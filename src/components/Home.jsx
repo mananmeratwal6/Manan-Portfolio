562ccd3bf7f334f6924b781b1bba60423503c52e
Manan Meratwal <mananmeratwal5@gmail.comgit config --global user.email mananmeratwal6@gmail.com>
2024-09-07 21:43:56 +0530

    portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,38 @@
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className="sectiontwo">
+      <h1 className='part'>Hello I'm MANAN</h1>
+      <p>I love to learn and build scalable and optimized backend systems.
+
+</p>
+<div class="area">
+  <ul class="circles">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+  <ul class="bubbles">
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+    <li></li>
+  </ul>
+</div>
+    </div>
+    
+    
+  )
+}
+
+export default Home
